64235b926c70ffbd467a0cc5ab9e7c15aff76a00
KhanhNhat242 <86726746+KhanhNhat242@users.noreply.github.com>
2024-03-12 17:20:23 +0700

    custom login, signup page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './components/home/Home'
 import Chat from './components/chat/Chat'
 import TopNav from './components/navigation/topNav/TopNav'
 import Profile from './components/profile/Profile'
+import Welcome from './components/login/Welcome'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/chat' element={<Chat />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/welcome' element={<Welcome />} />
       </Routes>
     </Router>
   )
---
+++ src/components/login/Welcome.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './Welcome.scss'
+
+const Welcome = () => {
+    const [isSignUp, setIsSignUp] = useState(false);
+
+    const toggleSignUp = () => {
+        setIsSignUp(!isSignUp);
+    };
+
+    return (
+        <div className={`cont ${isSignUp ? 's--signup' : ''}`}>
+            <div className="form sign-in">
+                <h2>Welcome</h2>
+                <label>
+                    <span>Số điện thoại</span>
+                    <input type="text" />
+                </label>
+                <label>
+                    <span>Password</span>
+                    <input type="password" />
+                </label>
+                <p className="forgot-pass">Forgot password?</p>
+                <button type="button" className="submit">Đăng nhập</button>
+            
+            </div>
+            <div className="sub-cont">
+                <div className="img">
+                    <div className="img__text m--up">
+                        <h3>Bạn chua có tài khoản? Vui lòng Đăng ký!</h3>
+                    </div>
+                    <div className="img__text m--in">
+                        <h3>Nếu bạn đã có tài khoản, hãy đăng nhập.</h3>
+                    </div>
+                    <div className="img__btn" onClick={() => toggleSignUp()}>
+                        <span className="m--up">Đăng ký</span>
+                        <span className="m--in">Đăng nhập</span>
+                    </div>
+                </div>
+                <div className="form sign-up">
+                    <h2>Tạo tài khoản mới</h2>
+                    <label>
+                        <span>Số điện thoại</span>
+                        <input type="text" />
+                    </label>
+                    <label>
+                        <span>Mật khẩu</span>
+                        <input type="password" />
+                    </label>
+                    <label>
+                        <span>Nhập lại mật khẩu</span>
+                        <input type="password" />
+                    </label>
+                    <button type="button" className="submit">Đăng ký</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
